51aca2a72b7e1b51a3751d68416b2d0aeffd40d9
Yesen Muu <1320563812@qq.com>
2024-05-05 00:58:55 +0900

    update the causal class with pc and ges.

--- tests/test_causal.py
+++ tests/test_causal.py
@@ -24,11 +24,11 @@ class TestCuasal(unittest.TestCase):
     def test_causal_graph_pc(self):
         data = Query.read_csv('./data/600036SH.csv', begin='2020-01-01', end='2023-06-21')
         data = data.drop(columns=['code'])
-        Graph.fit_causal(data, method='pc', is_discrete=False)
+        Graph.fit_causal(data, method='pc', is_discrete=False, filename='res/pc.png')
 
     def test_causal_graph_ges(self):
         data = Query.read_csv('./data/600036SH.csv', begin='2020-01-01', end='2023-06-21')
         data = data.drop(columns=['code'])
-        Graph.fit_causal(data, method='ges', is_discrete=False)
+        Graph.fit_causal(data, method='ges', is_discrete=False, filename='res/ges.png')
 
 
