fa0d6075b312504b74e815ea8dcb164c31a1d6db
Yesen Muu <1320563812@qq.com>
2024-08-17 00:53:36 +0900

    adjust the project's structure.

--- tradelearn/trader/__init__.py
+++ tradelearn/trader/__init__.py
@@ -1,2 +1,5 @@
-from .core import Backtest, Strategy
+from ._plotting import set_bokeh_output  # noqa: F401
+from .backtesting import Backtest, Strategy  # noqa: F401
 from .utils import *
+from . import lib  # noqa: F401
+
