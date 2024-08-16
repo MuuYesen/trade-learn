5f08760e1d4ef055ad95f755c204706b501eb7a3
Yesen Muu <1320563812@qq.com>
2024-08-16 23:48:29 +0900

    add a requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 import setuptools
 
+from os import path as os_path
+this_directory = os_path.abspath(os_path.dirname(__file__))
+def read_file(filename):
+    desc = ''
+    with open(os_path.join(this_directory, filename), encoding='utf-8') as f:
+        desc = f.read()
+    return desc
+def read_requirements(filename):
+    return [line.strip() for line in read_file(filename).splitlines()
+            if (not line.startswith('#')) and ('==' in line)]
+
+
 with open('README.md', 'r') as fh:
     README = fh.read()
 
@@ -12,21 +24,7 @@ setuptools.setup(
     description='trade-learn Python Package',
     long_description=README,
     long_description_content_type='text/markdown',
-    install_requires=[
-        'numpy',
-        'pandas',
-        'scipy',
-        'statsmodels',
-        'scikit-learn',
-        'seaborn',
-        'bokeh',
-        'yfinance',
-        'mootdx',
-        'tvDatafeed',
-        'pydot',
-        'quantstats',
-        'htmlmin'
-    ],
+    install_requires=read_requirements('requirements.txt'),
     url='https://github.com/MuuYesen/trade-learn',
     packages=setuptools.find_namespace_packages(),
     package_data={'': ['*.json', '*.html', '*.js', '*.css', '*.j2']},
